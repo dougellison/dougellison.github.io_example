fa04347c1203f2e8390b95d1e62e4e4c5eb29fbb
dougellison <doug.ellison@gmail.com>
2014-11-23 14:24:28 -0700

    First commit with AngularJS app for CS5371 Demo Project

---
+++ js/mainApp.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Home on 11/23/2014.
+ */
+
+var myApp = angular.module('myApp', []);
+
+
+
+myApp.controller('MainApp', function($scope) {
+    $scope.helloWorld = 'hello there';
+})
